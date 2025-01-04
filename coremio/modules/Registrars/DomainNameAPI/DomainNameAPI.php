01886042a8129cab58e70874b89053f0c1ace753
xa81 <poisonkid@gmail.com>
2025-01-04 14:56:52 +0300

    BUG-5018 : WISECP tarafında hata hakkında - 3674704

--- coremio/modules/Registrars/DomainNameAPI/DomainNameAPI.php
+++ coremio/modules/Registrars/DomainNameAPI/DomainNameAPI.php
@@ -867,7 +867,7 @@ class DomainNameAPI {
 
         if ($currentstatus == "Active") {
             $dns = [];
-
+/*
             if (isset($params["ns1"]) && $params["ns1"]) {
                 $dns[] = $params["ns1"];
             }
@@ -883,7 +883,7 @@ class DomainNameAPI {
 
             $this->ModifyDns(['domain' => $domain], $dns);
             $this->ModifyWhois(['domain' => $domain], $params["whois"]);
-
+*/
 
             $return_data["status"] = "active";
         } elseif ($currentstatus == "Expired") {
