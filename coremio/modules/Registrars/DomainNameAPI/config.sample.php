011b5d75e05f35aae155c672504778a67d8e32ee
xa81 <poisonkid@gmail.com>
2026-07-10 10:40:31 +0300

    Bump version to 2.0.10

--- coremio/modules/Registrars/DomainNameAPI/config.sample.php
+++ coremio/modules/Registrars/DomainNameAPI/config.sample.php
@@ -3,7 +3,7 @@
 return [
     'meta'     => [
         'name'    => 'DomainNameAPI',
-        'version' => '2.0.9',
+        'version' => '2.0.10',
         'logo'    => 'logo.png',
     ],
     'settings' => [
